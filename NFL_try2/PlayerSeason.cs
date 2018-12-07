3a98164ba1a82015a42dcb6352ae49f735c68aff
McCombs <Randy.McCombs@safelite.com>
2018-12-07 11:54:31 -0500

    test
    asdfasd

---
+++ NFL_try2/PlayerSeason.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NFL_try2
+{
+    class PlayerSeason
+    {
+        //test
+        public string Player { get; set; }
+        public string Position { get; set; }
+        public double PassingYards { get; set; }
+        public double RushingYards { get; set; }
+        //collections
+        //copypasta
+
+    }
+}
